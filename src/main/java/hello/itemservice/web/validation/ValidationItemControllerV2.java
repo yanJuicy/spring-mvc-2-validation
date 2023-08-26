5362fa1fd82c57f7d159025e565010ad15f3d333
yanJuicy <hanjub3@gmail.com>
2023-08-26 21:41:04 +0900

    Validator 분리1

--- src/main/java/hello/itemservice/web/validation/ValidationItemControllerV2.java
+++ src/main/java/hello/itemservice/web/validation/ValidationItemControllerV2.java
@@ -6,14 +6,8 @@ import lombok.RequiredArgsConstructor;
 import lombok.extern.slf4j.Slf4j;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
-import org.springframework.util.StringUtils;
 import org.springframework.validation.BindingResult;
-import org.springframework.validation.ValidationUtils;
-import org.springframework.web.bind.annotation.GetMapping;
-import org.springframework.web.bind.annotation.ModelAttribute;
-import org.springframework.web.bind.annotation.PathVariable;
-import org.springframework.web.bind.annotation.PostMapping;
-import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.*;
 import org.springframework.web.servlet.mvc.support.RedirectAttributes;
 
 import java.util.List;
@@ -25,80 +19,59 @@ import java.util.List;
 @Slf4j
 public class ValidationItemControllerV2 {
 
-	private final ItemRepository itemRepository;
+    private final ItemRepository itemRepository;
+    private final ItemValidator itemValidator;
 
-	@GetMapping
-	public String items(Model model) {
-		List<Item> items = itemRepository.findAll();
-		model.addAttribute("items", items);
-		return "validation/v2/items";
-	}
+    @GetMapping
+    public String items(Model model) {
+        List<Item> items = itemRepository.findAll();
+        model.addAttribute("items", items);
+        return "validation/v2/items";
+    }
 
-	@GetMapping("/{itemId}")
-	public String item(@PathVariable long itemId, Model model) {
-		Item item = itemRepository.findById(itemId);
-		model.addAttribute("item", item);
-		return "validation/v2/item";
-	}
+    @GetMapping("/{itemId}")
+    public String item(@PathVariable long itemId, Model model) {
+        Item item = itemRepository.findById(itemId);
+        model.addAttribute("item", item);
+        return "validation/v2/item";
+    }
 
-	@GetMapping("/add")
-	public String addForm(Model model) {
-		model.addAttribute("item", new Item());
-		return "validation/v2/addForm";
-	}
+    @GetMapping("/add")
+    public String addForm(Model model) {
+        model.addAttribute("item", new Item());
+        return "validation/v2/addForm";
+    }
 
-	@PostMapping("/add")
-	public String addItem(@ModelAttribute Item item, BindingResult bindingResult,
-						  RedirectAttributes redirectAttributes) {
+    @PostMapping("/add")
+    public String addItem(@ModelAttribute Item item, BindingResult bindingResult,
+                          RedirectAttributes redirectAttributes) {
+        itemValidator.validate(item, bindingResult);
 
-		log.info("objectName={}", bindingResult.getObjectName());
-		log.info("target={}", bindingResult.getTarget());
+        // 검증에 실패하면 다시 입력 폼으로
+        if (bindingResult.hasErrors()) {
+            log.info("errors={}", bindingResult);
+            return "validation/v2/addForm";
+        }
 
-		if (!StringUtils.hasText(item.getItemName())) {
-			ValidationUtils.rejectIfEmptyOrWhitespace(bindingResult, "itemName", "required");
-		}
-		if (item.getPrice() == null || item.getPrice() < 1000 || item.getPrice() >
-				1000000) {
-			bindingResult.rejectValue("price", "range", new Object[]{1000, 1000000}, null);
-		}
-		if (item.getQuantity() == null || item.getQuantity() >= 9999) {
-			bindingResult.rejectValue("quantity", "max", new Object[]{9999}, null);
+        // 성공 로직
+        Item savedItem = itemRepository.save(item);
+        redirectAttributes.addAttribute("itemId", savedItem.getId());
+        redirectAttributes.addAttribute("status", true);
+        return "redirect:/validation/v2/items/{itemId}";
+    }
 
-		}
+    @GetMapping("/{itemId}/edit")
+    public String editForm(@PathVariable Long itemId, Model model) {
+        Item item = itemRepository.findById(itemId);
+        model.addAttribute("item", item);
+        return "validation/v2/editForm";
+    }
 
-		// 특정 필드가 아닌 전체 예외
-		if (item.getPrice() != null && item.getQuantity() != null) {
-			int resultPrice = item.getPrice() * item.getQuantity();
-			if (resultPrice < 10000) {
-				bindingResult.reject("totalPriceMin", new Object[]{10000, resultPrice}, null);
-			}
-		}
-
-		// 검증에 실패하면 다시 입력 폼으로
-		if (bindingResult.hasErrors()) {
-			log.info("errors={}", bindingResult);
-			return "validation/v2/addForm";
-		}
-
-		// 성공 로직
-		Item savedItem = itemRepository.save(item);
-		redirectAttributes.addAttribute("itemId", savedItem.getId());
-		redirectAttributes.addAttribute("status", true);
-		return "redirect:/validation/v2/items/{itemId}";
-	}
-
-	@GetMapping("/{itemId}/edit")
-	public String editForm(@PathVariable Long itemId, Model model) {
-		Item item = itemRepository.findById(itemId);
-		model.addAttribute("item", item);
-		return "validation/v2/editForm";
-	}
-
-	@PostMapping("/{itemId}/edit")
-	public String edit(@PathVariable Long itemId, @ModelAttribute Item item) {
-		itemRepository.update(itemId, item);
-		return "redirect:/validation/v2/items/{itemId}";
-	}
+    @PostMapping("/{itemId}/edit")
+    public String edit(@PathVariable Long itemId, @ModelAttribute Item item) {
+        itemRepository.update(itemId, item);
+        return "redirect:/validation/v2/items/{itemId}";
+    }
 
 }
 
